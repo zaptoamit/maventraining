f8be515084ed6a7a16e3ec67e7abbb26a3cd05f8
zaptoamit <37148304+zaptoamit@users.noreply.github.com>
2021-03-07 13:20:25 +0800

    updated pom and hello world

--- src/main/java/com/maventraning/firstproject/HelloWorld.java
+++ src/main/java/com/maventraning/firstproject/HelloWorld.java
@@ -2,6 +2,6 @@ package com.maventraning.firstproject;
 
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.print("Hello world");
+        System.out.println("Hello world");
     }
 }
